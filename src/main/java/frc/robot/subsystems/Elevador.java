1068a072d30c91f703874e927d6031ac8ec9f8f7
Nicolas4532 <dnicolasvm@gmail.com>
2025-09-02 09:20:07 -0700

    prueba commit

--- src/main/java/frc/robot/subsystems/Elevador.java
+++ src/main/java/frc/robot/subsystems/Elevador.java
@@ -9,40 +9,47 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public  class Elevador extends SubsystemBase {
-    
-    public SparkMax elevadorMotorLeader; //elevador cims en toughbox
-    public SparkMax elevadorMotorFollower; //elevador cim en toughbox
+public class Elevador extends SubsystemBase {
+
+    public SparkMax elevadorMotorLeader; // elevador cims en toughbox
+    public SparkMax elevadorMotorFollower; // elevador cim en toughbox
     public DigitalInput Limit1;
+
     public Elevador() {
 
-    elevadorMotorLeader = new SparkMax(6, MotorType.kBrushed);
-    elevadorMotorFollower = new SparkMax(7, MotorType.kBrushed);
-    Limit1 = new DigitalInput(0);
+        elevadorMotorLeader = new SparkMax(6, MotorType.kBrushed);
+        elevadorMotorFollower = new SparkMax(7, MotorType.kBrushed);
+        Limit1 = new DigitalInput(0);
 
-    SparkMaxConfig baseConfig = new SparkMaxConfig();
-    SparkMaxConfig elevadorMotorFollowerConfig = new SparkMaxConfig();
+        SparkMaxConfig baseConfig = new SparkMaxConfig();
+        SparkMaxConfig elevadorMotorFollowerConfig = new SparkMaxConfig();
 
-    baseConfig.idleMode(IdleMode.kBrake);
+        baseConfig.idleMode(IdleMode.kBrake);
 
-    elevadorMotorFollowerConfig
-    .apply(baseConfig)
-    .follow(elevadorMotorLeader);
+        elevadorMotorFollowerConfig
+                .apply(baseConfig)
+                .follow(elevadorMotorLeader);
 
-    elevadorMotorLeader.configure(baseConfig,com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    elevadorMotorFollower.configure(elevadorMotorFollowerConfig,com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-}
-    public void subir() {
-        elevadorMotorLeader.set(1); 
+        elevadorMotorLeader.configure(baseConfig, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters,
+                PersistMode.kNoPersistParameters);
+        elevadorMotorFollower.configure(elevadorMotorFollowerConfig,
+                com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
     }
+
+    public void subir() {
+        elevadorMotorLeader.set(1);
+    }
+
     public void detenerse() {
         elevadorMotorLeader.set(0);
     }
+
     public void bajar() {
-        elevadorMotorLeader.set(-.65); 
+        elevadorMotorLeader.set(-.65);
     }
 
     public boolean Abajo() {
         return !Limit1.get();
     }
+    // p
 }
--- src/main/java/frc/robot/subsystems/Lifter.java
+++ src/main/java/frc/robot/subsystems/Lifter.java
@@ -7,35 +7,40 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public  class Lifter extends SubsystemBase {
-    
-    public SparkMax lifterMotorLeader; //neo en toughbox
-    public SparkMax lifterMotorFollower; //neo en toughbox
+public class Lifter extends SubsystemBase {
+
+    public SparkMax lifterMotorLeader; // neo en toughbox
+    public SparkMax lifterMotorFollower; // neo en toughbox
+
     public Lifter() {
 
-    lifterMotorLeader = new SparkMax(8, MotorType.kBrushless);
-    lifterMotorFollower = new SparkMax(9, MotorType.kBrushless);
+        lifterMotorLeader = new SparkMax(8, MotorType.kBrushless);
+        lifterMotorFollower = new SparkMax(9, MotorType.kBrushless);
 
-    SparkMaxConfig baseConfig = new SparkMaxConfig();
-    //SparkMaxConfig lifterrMotorLeaderConfig = new SparkMaxConfig();
-    SparkMaxConfig lifterMotorFollowerConfig = new SparkMaxConfig();
+        SparkMaxConfig baseConfig = new SparkMaxConfig();
+        SparkMaxConfig lifterMotorFollowerConfig = new SparkMaxConfig();
 
-    baseConfig.idleMode(IdleMode.kBrake);
+        baseConfig.idleMode(IdleMode.kBrake);
 
-    lifterMotorFollowerConfig
-    .apply(baseConfig)
-    .follow(lifterMotorLeader);
+        lifterMotorFollowerConfig
+                .apply(baseConfig)
+                .follow(lifterMotorLeader);
 
-    lifterMotorLeader.configure(baseConfig,com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    lifterMotorFollower.configure(lifterMotorFollowerConfig,com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-}
-public void lift() {
-    lifterMotorLeader.set(-1); 
-}
-public void hold() {
-    lifterMotorLeader.set(0);
-}
-public void abajo() {
-    lifterMotorLeader.set(1); 
-}
+        lifterMotorLeader.configure(baseConfig, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters,
+                PersistMode.kNoPersistParameters);
+        lifterMotorFollower.configure(lifterMotorFollowerConfig,
+                com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    }
+
+    public void lift() {
+        lifterMotorLeader.set(-1);
+    }
+
+    public void hold() {
+        lifterMotorLeader.set(0);
+    }
+
+    public void abajo() {
+        lifterMotorLeader.set(1);
+    }
 }
